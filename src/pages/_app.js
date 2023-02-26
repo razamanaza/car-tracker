b4586be3e6b7a5d10184c663bc49e97b736ac629
Aleksandr Gordeev <razamanaza@gmail.com>
2023-02-26 19:59:21 +1300

    Add MUI components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
 import '@/styles/globals.css';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '700'],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${roboto.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
