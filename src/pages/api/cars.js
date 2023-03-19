8cc97511f79b8916a3c5b5a988341e8459eb3605
Aleksandr Gordeev <razamanaza@gmail.com>
2023-03-19 13:24:00 +1300

    Add cache

--- src/pages/api/cars.js
+++ src/pages/api/cars.js
@@ -1,14 +1,41 @@
 import traders from '@/data/traders';
+import fs from 'fs/promises';
+import path from 'path';
 
 export default async function fetchTrueValue(req, res) {
-  const promises = traders.map(async (trader) => {
-    const remote = await fetch(trader.scrapeLink);
-    const html = await remote.text();
-    const vehicle = trader.getVehicleData(html);
-    return vehicle;
-  });
+  try {
+    const cacheDir = path.join(__dirname, 'cache');
+    const cacheFile = path.join(cacheDir, 'data.json');
+    await fs.mkdir(cacheDir, { recursive: true });
+    let lastChangeDate = 0;
+    try {
+      const { mtime } = await fs.stat(cacheFile);
+      lastChangeDate = mtime;
+    } catch {
+      console.log('No file');
+    }
+    const dateDiff = Date.now() - lastChangeDate;
 
-  const data = (await Promise.all(promises)).flat();
+    let data = {};
+    if (dateDiff > 86400) {
+      const promises = traders.map(async (trader) => {
+        const remote = await fetch(trader.scrapeLink);
+        const html = await remote.text();
+        const vehicle = trader.getVehicleData(html);
+        return vehicle;
+      });
+      data = (await Promise.all(promises)).flat();
+      await fs.writeFile(cacheFile, JSON.stringify(data));
+      console.log('Read from internet');
+    } else {
+      const text = await fs.readFile(cacheFile);
+      data = JSON.parse(text);
+      console.log('Read from cache');
+    }
 
-  res.status(200).json({ data });
+    res.status(200).json({ data });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error });
+  }
 }
