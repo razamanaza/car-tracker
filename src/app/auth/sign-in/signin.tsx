f567c5a3466387fc5dafee72786ee818d2c73459
Aleksandr Gordeev <razamanaza@gmail.com>
2024-10-10 00:03:05 +1300

    Add shadcn ui

--- src/app/auth/sign-in/signin.tsx
+++ src/app/auth/sign-in/signin.tsx
@@ -1,7 +1,22 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
 export default function SignInPage() {
   return (
-    <section className="grid h-full items-center">
-      <div className=""></div>
+    <section className="grid min-h-svh place-content-center">
+      <Card className="w-52">
+        <CardHeader>Sign in</CardHeader>
+        <CardContent>
+          <Button>Google Sign In</Button>
+        </CardContent>
+      </Card>
     </section>
   );
 }
